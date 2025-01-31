7daece017913c9b1bfd384f3b0c8f7f87fb21bac
Zohebkazi <kazizoheb59@gmail.com>
2025-01-31 13:10:14 +0530

    Application Complete

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ import sys
 
 if __name__ == "__main__":
     try:
-        trainingpiprlineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(trainingpiprlineconfig)
+        trainingpipelineconfig = TrainingPipelineConfig()
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion Completed")
         print(dataingestionartifact)
-        data_validation_config = DataValidationConfig(trainingpiprlineconfig)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate the data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed") 
         print(data_validation_artifact) 
-        data_transformation_config = DataTransformationConfig(trainingpiprlineconfig)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
         logging.info('Data Transformation Started')
         data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
         data_transsformation_artifact = data_transformation.initiate_data_transformation()
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         print(data_transsformation_artifact)
         
         logging.info("Model training Started")
-        model_trainer_config = ModelTrainerConfig(trainingpiprlineconfig)
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transsformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
         logging.info("Model Training Artifact Created ")
